c4e4dbef7a63e05ef2562127ea2046f4762cfca7
mostafa-etman <56475185+mostafa-etman@users.noreply.github.com>
2021-05-26 02:51:23 +0200

    Right

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
               ),
             );
           } else   {
-            return Dashboard(FlutterBluetoothSerial.instance);
+            return Dashboard();
           }
         },
         // child: MyHomePage(title: 'Flutter Demo Home Page'),
